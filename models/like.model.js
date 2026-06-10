d71a192428e9672192e4631bb22e66d84b0e989e
sand7348 <bharnisandeep63@gmail.com>
2026-06-10 21:44:32 +0530

    followers

--- models/like.model.js
+++ models/like.model.js
@@ -5,12 +5,12 @@ const Post=require("./post.models")
 
 const likeSchema=new mongoose.Schema({
     post_id:{
-        type:mongoose.Schema.Types.ObjectId,
+        type:ObjectId,
         ref:"Post",
         required:[true,"post_id is required"]
     },
-    user_id:{
-        type:mongoose.Schema.Types.ObjectId,
+    user:{
+        type:ObjectId,
         ref:"User",
         required:[true,"user_id is required"]
     }
