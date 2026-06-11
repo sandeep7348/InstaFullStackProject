36108932b1f2b1c90ed297cc64b426655ff427cc
sand7348 <bharnisandeep63@gmail.com>
2026-06-11 18:03:03 +0530

    updated code

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,30 +3,47 @@ const router=express.Router()
 const User=require("../models/user.model.js")
 const bcrypt=require("bcrypt")
 const jwt=require("jsonwebtoken")
-router.post("/register",async(req,res)=>{
-    const {username,email,password,bio,profile_url}=req.body
-    const user=new User({
-        username,
-        email,
-        password,
-        bio,
-        profile_url
-    })
-    const salt=await bcrypt.genSalt(10)
-    user.password=await bcrypt.hash(user.password,salt)
-     
-    const savedUser =await user.save()
+router.post("/register", async (req, res) => {
+  try {
+    const { username, email, password, bio, profile_url } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({
+        message: "Email already exists",
+      });
+    }
+
+    const user = new User({
+      username,
+      email,
+      password,
+      bio,
+      profile_url,
+    });
+
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(password, salt);
+
+    const savedUser = await user.save();
+
     res.status(201).json({
-        message:"user registered successfully",
-        user:{
-            id:savedUser._id,
-            username:savedUser.username,
-            email:savedUser.email
-        }
-    })
+      message: "User registered successfully",
+      user: {
+        id: savedUser._id,
+        username: savedUser.username,
+        email: savedUser.email,
+      },
+    });
+  } catch (error) {
+    console.error("REGISTER ERROR:", error);
 
-})
-
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+});
 router.post("/login",async(req,res)=>{
     const {email,password}=req.body
     const user=await User.findOne({email})
